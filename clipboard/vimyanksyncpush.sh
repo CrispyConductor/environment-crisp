7be52c21fe8a9116d7f9214fec30179bbbaf040b
Chris Breneman <crispy@cluenet.org>
2021-01-04 17:30:59 -0500

    More readme

--- clipboard/vimyanksyncpush.sh
+++ clipboard/vimyanksyncpush.sh
@@ -2,18 +2,25 @@
 
 # Load buffer into tmux
 TEMPFILE="`tempfile 2>/dev/null`"
+MYDIR="$(realpath "$(dirname "$0")")"
 if [ $? -ne 0 ]; then
 	TEMPFILE="/tmp/_clip_temp_yssh$USER"
 fi
+export TEMPFILE
+export MYDIR
 cat > "$TEMPFILE"
+
+(
+
 tmux load-buffer "$TEMPFILE"
 
 # Trigger other running vims to pull in update
-MYDIR="$(realpath "$(dirname "$0")")"
 "$MYDIR/updatevims.sh"
 
-# Push to connected clipsyncd's
-"$MYDIR/clipsyncd_propagate.sh" &
-
 rm -f "$TEMPFILE"
+
+# Push to connected clipsyncd's
+"$MYDIR/clipsyncd_propagate.sh"
+
+) &
 
