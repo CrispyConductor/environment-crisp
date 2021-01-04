a9d974fa6257f73a4d7c19fb5bc60fd23726bcd2
Chris Breneman <crispy@cluenet.org>
2021-01-03 19:29:27 -0500

    More fallbacks

--- clipboard/updatevims.sh
+++ clipboard/updatevims.sh
@@ -2,5 +2,6 @@
 
 # Sends USR1 signal to running vims to cause them to pull in a clipboard update
 
-killall -q -s USR1 -u `whoami` nvim vim
+killall -USR1 -u `whoami` nvim vim &>/dev/null
+exit 0
 
--- clipboard/vimyanksyncpull.sh
+++ clipboard/vimyanksyncpull.sh
@@ -7,7 +7,9 @@ MAX_TIME_DIFF=3
 # don't output if buffer too large
 MAX_SIZE=100000000
 
-csv="`tmux list-buffers -F '#{buffer_created},#{buffer_size},#{buffer_name}' -f '#{m:buffer*,#{buffer_name}}'`"
+# this line uses tmux's built-in filtering which is only available in bleeding versions
+#csv="`tmux list-buffers -F '#{buffer_created},#{buffer_size},#{buffer_name}' -f '#{m:buffer*,#{buffer_name}}'`"
+csv="`tmux list-buffers -F '#{buffer_created},#{buffer_size},#{buffer_name}' | grep ',buffer[0-9]'`"
 if [ $? -ne 0 ]; then exit 1; fi
 csv="`echo "$csv" | head -n1`"
 if [ -z "$csv" ]; then exit 1; fi
