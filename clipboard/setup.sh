5fa9ff234163fdf75c9f5d1c6d86a5f23297a473
Chris Breneman <crispy@cluenet.org>
2021-01-05 20:19:35 -0500

    Don't remove sockets on setup

--- clipboard/setup.sh
+++ clipboard/setup.sh
@@ -9,9 +9,10 @@ mkdir -p "$BASEDIR"
 mkdir -p "$INBOUND"
 mkdir -p "$OUTBOUND"
 
-rm -f "$BASEDIR/clipsync.sock"
-rm -f "$INBOUND/*"
-rm -f "$OUTBOUND/*"
+# uncomment to remove old sockets - will disrupt running clipsyncd
+#rm -f "$BASEDIR/clipsync.sock"
+#rm -f "$INBOUND/*"
+#rm -f "$OUTBOUND/*"
 
 if [ ! -f "$HOSTFILE" ]; then
 	HOSTID="`hostname -f | sed 's/[^a-zA-Z0-9]/_/g'`_${RANDOM}"
