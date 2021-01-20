26e909dfe95c9e90dbddb931969fdaddd0ddb5e9
Chris Breneman <crispy@cluenet.org>
2021-01-19 19:44:43 -0500

    Add showkey utility

---
+++ utils/showkey.py
@@ -0,0 +1,23 @@
+# This listens for keypresses and echoes the bytes received.
+# It's from a stack overflow post online somewhere, but I can't seem to find it.
+import termios, fcntl, sys, os
+fd = sys.stdin.fileno()
+
+oldterm = termios.tcgetattr(fd)
+newattr = termios.tcgetattr(fd)
+newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+try:
+    while 1:
+        try:
+            c = sys.stdin.read(1)
+            if c:
+                print("Got character", repr(c))
+        except IOError: pass
+finally:
+    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
