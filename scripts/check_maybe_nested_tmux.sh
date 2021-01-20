4f5156d5524c0190a235254247f220b0ecf796c2
Chris Breneman <crispy@cluenet.org>
2021-01-19 22:45:15 -0500

    Easy nested pane switching

---
+++ scripts/check_maybe_nested_tmux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#pgrep -lt $(tmux list-panes -F "#{pane_active} #{pane_tty}" | grep -E "^1" | cut -d " " -f 2- | cut -d / -f 3-) | grep -E "ssh|tmux" &>/dev/null
+
+TTY="`tmux list-panes -F "#{pane_active} #{pane_tty}" | grep -E "^1" | cut -d " " -f 2- | cut -d / -f 3-`"
+if [ -z $TTY ]; then exit 1; fi
+ps -ao tty,comm | grep -E "^$TTY" | grep -E "ssh|tmux" &>/dev/null
+
+exit $?
+
