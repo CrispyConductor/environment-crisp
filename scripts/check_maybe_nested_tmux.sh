ebb883faa891c7064ee59676a79ee9a88a940352
Chris Breneman <crispy@cluenet.org>
2021-01-25 15:59:33 -0500

    Same fix for window switching

--- scripts/check_maybe_nested_tmux.sh
+++ scripts/check_maybe_nested_tmux.sh
@@ -3,7 +3,7 @@
 
 TTY="`tmux list-panes -F "#{pane_active} #{pane_tty}" | grep -E "^1" | cut -d " " -f 2- | cut -d / -f 3-`"
 if [ -z $TTY ]; then exit 1; fi
-ps -ao tty,comm | grep -E "^$TTY" | grep -E "ssh|tmux" &>/dev/null
+ps -ao tty,comm | grep -E "^${TTY}\\s" | grep -E "ssh|tmux" &>/dev/null
 
 exit $?
 
