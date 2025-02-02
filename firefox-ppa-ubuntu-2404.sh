fe9a439c065075906f7553aed731cda2d720513b
Chris Breneman <crispy@cluenet.org>
2025-02-02 14:53:02 -0500

    Add firefox apt script

---
+++ firefox-ppa-ubuntu-2404.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo snap remove firefox
+if [ $? -ne 0 ]; then exit 1; fi
+
+sudo apt remove firefox
+if [ $? -ne 0 ]; then exit 1; fi
+
+sudo add-apt-repository ppa:mozillateam/ppa
+if [ $? -ne 0 ]; then exit 1; fi
+
+cat | sudo tee /etc/apt/preferences.d/mozilla <<EOF
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1
+EOF
+
+sudo apt update
+if [ $? -ne 0 ]; then exit 1; fi
+
+sudo apt install firefox
+if [ $? -ne 0 ]; then exit 1; fi
+
