28149c450fb2e7167e757abda2b1c0672a823593
Chris Breneman <crispy@cluenet.org>
2025-03-02 21:40:22 -0500

    More ubuntu 2410

---
+++ packages-ubuntu-gui-2410.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal-xenial.list
+rm signal-desktop-keyring.gpg
+
+# regolith ppa
+wget -qO - https://regolith-desktop.org/regolith.key | gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://regolith-desktop.org/testing-ubuntu-oracular-amd64 oracular main" | sudo tee /etc/apt/sources.list.d/regolith.list
+
+sudo apt update
+
+packages="
+keepassxc
+nextcloud-desktop
+signal-desktop
+gdebi-core
+xclip
+python3-tk
+regolith-desktop
+regolith-session-flashback
+regolith-look-lascaille
+i3xrocks-weather
+i3xrocks-memory
+i3xrocks-volume
+i3xrocks-battery
+"
+
+#packages="
+#regolith-desktop
+#i3xrocks-weather
+#i3xrocks-memory
+#i3xrocks-volume
+#i3xrocks-battery
+#keepassxc
+#nextcloud-desktop
+#signal-desktop
+#gdebi-core
+#xclip
+#python3-tk
+#"
+
+
+lst=''
+for p in $packages; do
+	lst="$lst $p"
+done
+
+sudo apt-get install $lst
+
+# discord (depends on gdebi)
+mkdir -p ~/Downloads
+wget -O ~/Downloads/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo gdebi ~/Downloads/discord.deb
+
